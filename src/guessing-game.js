7ba9aae5754a9be326f90de933a4d24ab0a45331
NastyaMedv <33265018+NastyaMedv@users.noreply.github.com>
2019-10-10 01:37:57 +0300

    solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    var start = 0, end = 0, current = 0;
+    }
 
     setRange(min, max) {
-
+      this.start = min; this.end = max;
     }
 
     guess() {
-
+      return Math.ceil(this.start+((this.end-this.start)/2));
     }
 
     lower() {
-
+      this.end = Math.ceil(this.start+((this.end-this.start)/2));
     }
 
     greater() {
-
+      this.start = Math.ceil(this.start+((this.end-this.start)/2));
     }
 }
 
